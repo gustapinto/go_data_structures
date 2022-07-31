98817fc87e2b184c860222d561ee461738ddb920
Gustavo Henrique <gustavo.henrique@nappsolutions.com>
2022-07-31 11:34:55 -0300

    Add queue

---
+++ queue/queue.go
@@ -0,0 +1,76 @@
+package queue
+
+type QueueNode struct {
+	Value any
+	Next  *QueueNode
+}
+
+// Size retorna o tamanho da fila
+func Size(queue *QueueNode) uint {
+	if queue == nil {
+		return 0
+	}
+
+	temp := queue
+	counter := uint(0)
+
+	for temp != nil {
+		temp = temp.Next
+		counter++
+	}
+
+	return counter
+}
+
+// Push adiciona um novo item no final da fila
+func Push(queue *QueueNode, item any) *QueueNode {
+	if queue == nil {
+		return &QueueNode{Value: item}
+	}
+
+	if queue.Next == nil {
+		queue.Next = &QueueNode{Value: item}
+
+		return queue.Next
+	}
+
+	return Push(queue.Next, item)
+}
+
+// Pop removoe um elemento do começo da fila
+func Pop(queue *QueueNode) any {
+	if Size(queue) == 0 {
+		return nil
+	}
+
+	if Size(queue) == 1 {
+		value := queue.Value
+		// Quando a fila possui somente um item ao removê-lo a fila deve ser anulada, ou seja
+		// a fila deve seu "zero value"
+		*queue = QueueNode{}
+
+		return value
+	}
+
+	value := queue.Value
+	// Movimenta os elementos da fila quando o primeiro valor é removido
+	*queue = *queue.Next
+
+	return value
+}
+
+// Traverse retorna uma slice com todos os valores na fila
+func Traverse(queue *QueueNode) (values []any) {
+	if Size(queue) == 0 {
+		return []any{}
+	}
+
+	temp := queue
+
+	for temp != nil {
+		values = append(values, temp.Value)
+		temp = temp.Next
+	}
+
+	return values
+}
---
+++ queue/queue_test.go
@@ -0,0 +1,176 @@
+package queue
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSizeMustReturnZeroOnNil(t *testing.T) {
+	size := Size(nil)
+
+	if size != 0 {
+		t.Errorf("Failed! Expected size to be 0, got %d", size)
+	}
+}
+
+func TestSizeMustReturnTheSizeOfQueue(t *testing.T) {
+	queue := &QueueNode{
+		Value: 10,
+		Next: &QueueNode{
+			Value: 4,
+			Next: &QueueNode{
+				Value: -2,
+				Next:  nil,
+			},
+		},
+	}
+
+	size := Size(queue)
+
+	if size != 3 {
+		t.Errorf("Failed! Expected size to be 3, got %d", size)
+	}
+}
+
+func TestSizeMustNotMutateTheQueue(t *testing.T) {
+	queue := &QueueNode{
+		Value: 10,
+		Next: &QueueNode{
+			Value: 4,
+			Next: &QueueNode{
+				Value: -2,
+				Next:  nil,
+			},
+		},
+	}
+
+	original := queue
+
+	_ = Size(queue)
+
+	if !reflect.DeepEqual(queue, original) {
+		t.Errorf("Failed! Expected size to do not mutate the queue, got %v", queue)
+	}
+}
+
+func TestPushMustInitializeANewQueue(t *testing.T) {
+	expected := &QueueNode{
+		Value: 10,
+		Next:  nil,
+	}
+
+	queue := Push(nil, 10)
+
+	if !reflect.DeepEqual(queue, expected) {
+		t.Errorf("Failed! Expected %v, got %v", expected, queue)
+	}
+}
+
+func TestPushMustAddANewItem(t *testing.T) {
+	expected := &QueueNode{
+		Value: 10,
+		Next: &QueueNode{
+			Value: 4,
+			Next: &QueueNode{
+				Value: -2,
+				Next:  nil,
+			},
+		},
+	}
+
+	queue := Push(nil, 10)
+	Push(queue, 4)
+	Push(queue, -2)
+
+	if !reflect.DeepEqual(queue, expected) {
+		t.Errorf("Failed! Expected %v, got %v", expected, queue)
+	}
+}
+
+func TestPopMustReturnNilOnNil(t *testing.T) {
+	value := Pop(nil)
+
+	if value != nil {
+		t.Errorf("Failed! Expected nil, got %v", value)
+	}
+}
+
+func TestPopMustDefineQueueAsItZeroValueWhenItOnlyHaveOneItem(t *testing.T) {
+	expected := &QueueNode{}
+
+	queue := &QueueNode{
+		Value: 4,
+		Next:  nil,
+	}
+
+	_ = Pop(queue)
+
+	if !reflect.DeepEqual(queue, expected) {
+		t.Errorf("Failed! Expected %v, got %v", expected, queue)
+	}
+}
+
+func TestPopMustReturnTheFirstValue(t *testing.T) {
+	queue := &QueueNode{
+		Value: 10,
+		Next: &QueueNode{
+			Value: 4,
+			Next:  nil,
+		},
+	}
+
+	value := Pop(queue)
+
+	if value != 10 {
+		t.Errorf("Failed! Expected value to be 10, got %d", value)
+	}
+}
+
+func TestPopMustRemoveAItem(t *testing.T) {
+	expected := &QueueNode{
+		Value: 4,
+		Next:  nil,
+	}
+
+	queue := &QueueNode{
+		Value: 10,
+		Next: &QueueNode{
+			Value: 4,
+			Next:  nil,
+		},
+	}
+
+	_ = Pop(queue)
+
+	if !reflect.DeepEqual(queue, expected) {
+		t.Errorf("Failed! Expected %v, got %v", expected, queue)
+	}
+
+	size := Size(queue)
+
+	if size != 1 {
+		t.Errorf("Failed! Expected size to be 1, got %d", size)
+	}
+}
+
+func TestTraverseMustReturnAEmptySliceOnNi(t *testing.T) {
+	values := Traverse(nil)
+
+	if !reflect.DeepEqual(values, []any{}) {
+		t.Errorf("Failed! Expected a empty slice, got %v", values)
+	}
+}
+
+func TestTraverseMustRrturnTheQueueValues(t *testing.T) {
+	expected := []any{10, 4, 6}
+
+	queue := Push(nil, 10)
+	Push(queue, 4)
+	Push(queue, 6)
+
+	values := Traverse(queue)
+
+	if !reflect.DeepEqual(expected, values) {
+		t.Errorf("Failed! Expected %v, got %v", expected, values)
+	}
+}
